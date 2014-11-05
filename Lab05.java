1f380846a5b79c8b47c29dd2a16a2865421530ed
laviy <yonatan_lavi@asl.org>
2014-11-05 14:58:19 +0000

    lab05

--- Lab05.java
+++ Lab05.java
@@ -12,6 +12,24 @@ import edu.fcps.karel2.Robot;
 public class Lab05 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/shuttle.map");
+          Display.setSize(10, 10);
+          Display.setSpeed(5);
+          Racer karel = new Racer(1);
+          Racer bob = new Racer(4);
+          Racer tom = new Racer(7);
+          karel.shuttle(2, 7);
+          karel.shuttle(4, 5);
+          karel.shuttle(6, 3);
+          karel.move();
+          bob.shuttle(2, 7);
+          bob.shuttle(4, 5);
+          bob.shuttle(6, 3);
+          bob.move();
+          tom.shuttle(2, 7);
+          tom.shuttle(4, 5);
+          tom.shuttle(6, 3);
+          tom.move();
+           
      }
 }
--- Racer.java
+++ Racer.java
@@ -4,9 +4,56 @@
 *  @author <your name here, no brackets>
 *  @version <version or date, no brackets>
 */
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
 
 public class Racer extends Athlete {
-
-// consult the Karel API to determine what constructors and methods this class should implement
-
+   public Racer() {
+      super();
+      }
+   public Racer(int y) {
+      super(1, y, Display.EAST, Display.INFINITY);
+      }
+   public void jumpRight() {
+      turnLeft();
+      move();
+      turnRight();
+      move();
+      turnRight();
+      move();
+      turnLeft();
+      }
+   public void jumpLeft() {
+      turnRight();
+      move();
+      turnLeft();
+      move();
+      turnLeft();
+      move();
+      turnRight();
+      }
+   public void sprint(int n) {
+      for(int k=1; k <= n; k++)
+      move();
+      }
+   public void put(int n) {
+      for(int k=1; k <= n; k++)
+      putBeeper();
+      }
+   public void pick(int n) {
+      for(int k=1; k <= n; k++)
+      pickBeeper();
+      }
+   public void shuttle(int spaces, int beepers) {
+      move();
+      jumpRight();
+      sprint(spaces);
+      pick(beepers);
+      turnAround();
+      sprint(spaces);
+      jumpLeft();
+      move();
+      put(beepers);
+      turnAround();
+   }
 }
